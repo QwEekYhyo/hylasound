365490e69b6859df4a5e806cddf8930b181ef84e
Logan Lucas <logan.lucas83@gmail.com>
2024-07-01 19:34:51 +0200

    this was cool code but users couldn't spam sounds

--- src/soundbutton.cpp
+++ src/soundbutton.cpp
@@ -7,13 +7,7 @@
 SoundButton::SoundButton(QWidget* parent) : QPushButton(parent) {
     connect(this, &QPushButton::released, this, [&](){
         if (m_player && !m_path.isEmpty()) {
-#ifdef Q_OS_WIN
-            if (QString::compare(m_path, m_player->source().path(), Qt::CaseInsensitive) != 0) {
-#else
-            if (QString::compare(m_path, m_player->source().path(), Qt::CaseSensitive) != 0) {
-#endif
-                m_player->setSource(QUrl::fromLocalFile(m_path));
-            }
+            m_player->setSource(QUrl::fromLocalFile(m_path));
             m_player->play();
         }
     });
